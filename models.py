4e29984a200eb37e980187334500d2eda8510c76
joeng03 <yinjoeng2003@gmail.com>
2023-09-30 17:30:44 +0800

    checkpoint

--- models.py
+++ models.py
@@ -17,20 +17,19 @@ ROLE_PLAY_HUMAN_RESOURES_PROMPT = "As a professional human resources manager, gi
 openai.api_key = st.secrets["API_KEY"]
 
 def generate_recommendations(employee_data):
-    # content = ROLE_PLAY_HUMAN_RESOURES_PROMPT + parse_employee_data(employee_data)
-    # response = openai.Completion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[{"role":"user", "content": content}],
-    #     temperature=0.6
-    # )
-    # return {
-    #     "ai_recommendations": response.choices[0].message.content
-    # }
-    return parse_employee_data(employee_data)
+    content = ROLE_PLAY_HUMAN_RESOURES_PROMPT + parse_employee_data(employee_data)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role":"user", "content": content}],
+        temperature=0.6
+    )
+    return {
+        "ai_recommendations": response.choices[0].message.content
+    }
 
 def parse_employee_data(employee_data):
     data_json = json.dumps(employee_data.val(), indent=4)
     data_dict = json.loads(data_json)
     if 'skillsReview' not in data_dict:
         return ""
-    return "Name:{}\n Position:{}\n Skills Review:{}\n Education:{}".format(data_dict['name'], data_dict['position'], data_dict['skillsReview'], data_dict['education'])
+    return "Name:{}, Position:{}, Skills Review:{}, Education:{}".format(data_dict['name'], data_dict['position'], data_dict['skillsReview'], data_dict['education'])
