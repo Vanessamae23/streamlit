8f0af4be9a0f193a421b6648e66d890f2a8fbe60
joeng03 <yinjoeng2003@gmail.com>
2023-10-01 10:39:44 +0800

    feat: Add Skills -> Course recommendations

--- pages/ai_insights.py
+++ pages/ai_insights.py
@@ -6,23 +6,21 @@ from pyudemy import Udemy
 import streamlit.components.v1 as components
 from firebase import db
 from models import load_skills_recognition_pipeline
+from streamlit_card import card
 
 try :
-    st.set_page_config(page_title="AI Employee Analysis", page_icon = "✈️", layout = "centered", initial_sidebar_state = "auto")
+    st.set_page_config(layout="wide", page_title="AI Employee Analysis", page_icon = "✈️",  initial_sidebar_state = "auto")
     st.subheader("AI Employee Analysis")
-
-
 
 
     data = db.child('employees').get()
     json_data = json.dumps(data.val(), indent=4)
     json_dict = json.loads(json_data)
-    df = pd.DataFrame(json_dict.values())[['id', 'name', 'email', 'performance', 'points', 'skillsReview']]
+    df = pd.DataFrame(json_dict.values())[['id', 'name', 'email', 'skillsReview']]
 
     pipeline = load_skills_recognition_pipeline()
 
     df['Skills Interested'] = df['skillsReview'].apply(lambda x: ','.join(map(lambda x: x['word'], pipeline.group_entities(pipeline(x if pd.notnull(x) else "")))))
-
 
 
     th_props = [
@@ -46,6 +44,9 @@ try :
     df.style.set_properties(**{'text-align': 'left'}).set_table_styles(styles)
     st.table(df)
 
+    selected_employees = st.multiselect(
+        "Select Employees",
+        df['name'])
 
     udemy = Udemy(st.secrets['UDEMY_CLIENT_ID'], st.secrets['UDEMY_CLIENT_SECRET'])
     example = [
@@ -54,13 +55,28 @@ try :
             "Setting": "@min"
         }
     ]
-    data =(udemy.courses(page=1, page_size=10, search='java'))
 
-    for i, val in enumerate(data['results']) :
-        uri = "https://www.udemy.com/" + val['url']
-        st.write("Check out this ", uri)
-        st.image(val['image_240x135'])
-        st.text(val['title'])
+    udemy_search_keywords = df.loc[df['name'].isin(selected_employees)]['Skills Interested'].apply(lambda x: x.split(',')).explode().unique().tolist()
+
+    data = (udemy.courses(page=1, page_size=10, search=udemy_search_keywords))
+
+
+    for val in data['results'][:10] :
+        card(
+            title = val['title'],
+            text = val['headline'],
+            image = val['image_240x135'],
+            url = "https://www.udemy.com" + val['url'],
+            styles={
+            "card": {
+                "padding": "0",
+                "width": "300px",
+                "height": "300px"
+                # "border-radius": "60px",
+                # "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+                }
+            }
+        )
     
 except Exception as e :
     st.error(e)
